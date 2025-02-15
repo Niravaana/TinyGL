ba04343303c5512701e013d98e85728ec5464431
Paritosh Kulkarni <paritosh.kulkarni24@gmail.com>
2025-02-14 17:39:42 -0800

    Add initial framework

---
+++ src/OpenGL/core.cpp
@@ -0,0 +1,36 @@
+#include <Windows.h>
+#include "gl.h"
+#include "Context.h"
+
+void glBegin(GLenum mode)
+{
+}
+
+void glClear(GLbitfield mask)
+{
+}
+
+void glColor3f(GLfloat red, GLfloat green, GLfloat blue)
+{
+}
+
+void glEnd(void)
+{
+}
+
+void glFinish(void)
+{
+}
+
+void glFlush(void)
+{
+}
+
+GLenum glGetError(void)
+{
+	return GLenum();
+}
+
+void glVertex2f(GLfloat x, GLfloat y)
+{
+}
